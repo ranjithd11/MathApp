cd4ddb4653a5d8278c26020235f4f014038ca947
Ranjithkumar <ranjithkumar.d@live.com>
2026-09-07 00:22:36 +0530

    Fix: GitHub Pages base path and router basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <ThemeProvider>
       <SettingsProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/MathApp">
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
